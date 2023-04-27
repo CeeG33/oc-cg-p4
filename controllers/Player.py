a4bc1daf68b24fe221d7e0e2bc59287efdf4fc2f
Ciran GÜRBÜZ <ciran.gurbuz@gmail.com>
2023-04-27 13:05:59 +0200

    Ecriture du module Player

--- controllers/Player.py
+++ controllers/Player.py
@@ -9,8 +9,26 @@ class Player:
         ● Prénom
         ● Date de naissance
     """
+    rank = "Non défini"
     def __init__(self, name, first_name, date_of_birth):
         self.name = name
         self.first_name = first_name
         self.date_of_birth = date_of_birth
     pass
+
+joueur1 = Player("Etoile de mer", "Patrick", "01/02/2000")
+joueur2 = Player("L'Éponge", "Bob", "05/06/2002")
+
+"""
+print("Le joueur 1 est : ")
+print(f"Nom : {joueur1.name}")
+print(f"Prénom : {joueur1.first_name}")
+print(f"Date de naissance : {joueur1.date_of_birth}")
+print(f"Rang : {joueur1.rank}")
+print()
+print("Le joueur 2 est : ")
+print(f"Nom : {joueur2.name}")
+print(f"Prénom : {joueur2.first_name}")
+print(f"Date de naissance : {joueur2.date_of_birth}")
+print(f"Rang : {joueur2.rank}")
+"""
