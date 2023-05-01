730eeb7249dc17a5286dc8cc4face58b4c7e85c5
Ciran GÜRBÜZ <ciran.gurbuz@gmail.com>
2023-05-01 12:40:49 +0200

    Renommage des modules  + Ecriture de rounds

--- models/rounds.py
+++ models/rounds.py
@@ -12,13 +12,23 @@ class Round:
     comme terminé.
     """
 
+    match_list = []
+    start_date = []
+    end_date = []
+
     def __init__(self, round_name):
         self.round_name = round_name
 
     pass
 
 """
-    ">>> A déplacer dans AddContestToRoundsUn match unique doit être stocké sous la forme d'un tuple contenant deux 
+    ">>> A déplacer dans EndContest : 
+    Un match unique doit être stocké sous la forme d'un tuple contenant deux 
     listes, chacune contenant deux éléments : un joueur et un score. Les matchs doivent être stockés sous
     forme de liste dans l'instance du tour auquel ils appartiennent.
+    
+    Actuellement, nous appelons nos tours "Round 1", "Round 2", etc. Elle doit également
+    contenir un champ Date et heure de début et un champ Date et heure de fin, qui doivent
+    tous deux être automatiquement remplis lorsque l'utilisateur crée un tour et le marque
+    comme terminé.
 """
