ab83434915eebd26cb782f17e4f44b0d96e5f6b9
Ciran GÜRBÜZ <ciran.gurbuz@gmail.com>
2023-05-01 11:34:40 +0200

    Codage initial du modèle Contest

--- controllers/Contest.py
+++ controllers/Contest.py
@@ -4,6 +4,19 @@ class Contest:
     """
     ○ Chaque match consiste en une paire de joueurs
 
-
     """
-    pass
+    def __init__(self, player1, player2):
+        self.player1 = player1
+        self.player2 = player2
+
+
+match1 = Contest("Bob", "Patrick")
+
+
+"""
+print("Le match se compose des joueurs suivants :")
+print()
+print(f"Joueur 1 : {match1.player1}")
+print("contre")
+print(f"Joueur 2 : {match1.player2}")
+"""
