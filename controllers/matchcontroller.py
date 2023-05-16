504a4a819e01ab7705e3ceb4cb790827c7c5a334
Ciran GÜRBÜZ <ciran.gurbuz@gmail.com>
2023-05-16 17:53:55 +0200

    Codage contrôleur en cours

--- controllers/matchcontroller.py
+++ controllers/matchcontroller.py
@@ -1,5 +1,6 @@
 from models import match
 
+
 class MatchController:
     def __init__(self):
         self.match_list = []
@@ -12,3 +13,25 @@ class MatchController:
     def load_existing_match(self, player1, player2):
         existing_match = match.Match.create_from_json(player1, player2)
         self.match_list.append(existing_match)
+
+    def save_matches(self):
+        if len(self.match_list) >= 1:
+            for m in self.match_list:
+                m.update_json_file()
+            return "Matches saved successfully"
+        else:
+            return "There are no matches yet !"
+
+    def select_winner(self, match_index, player):
+        self.match_list[match_index].chose_winner(player)
+        self.match_list[match_index].update_json_file()
+
+    def declare_draw_match(self, match_index):
+        self.match_list[match_index].is_draw()
+        self.match_list[match_index].update_json_file()
+
+    def restore_match_result(self, match_index):
+        self.match_list[match_index].winner = None
+        self.match_list[match_index].draw = None
+        self.match_list[match_index].update_json_file()
+
